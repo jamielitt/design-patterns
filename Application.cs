62d2c5cc025169aa9a428435c544e79779920c69
Jamie Little <jamie.littledv@gmail.com>
2025-12-13 19:15:17 +0000

    Refactored to allow selection of pattern to run

--- Application.cs
+++ Application.cs
@@ -2,9 +2,31 @@ using static System.Console;
 namespace design_patterns;
 public class Application(IObserver observer)
 {
+    private List<string> options = new List<string>()
+    {
+        "1 - Observer"
+    };
+    
     public void Run()
     {
         WriteLine("Design Patterns example application");
-        observer.Run();
+        WriteLine("Please select which design pattern to execute:");
+        
+        foreach (var option in options)
+        {  
+            WriteLine(option);
+        }
+        
+        Write("> ");
+        var keyboardInput = ReadLine();
+        switch (keyboardInput)
+        {
+            case "1":
+                observer.Run();
+                break;
+            default:
+                WriteLine("Invalid input");
+                break;
+        }
     }
 }
