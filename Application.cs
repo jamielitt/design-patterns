3c368a210a5f2eda5715cfbb222bf6c782f6212f
jamielitt <jamie.littledv@gmail.com>
2026-01-03 17:53:49 +0000

    Refactored main application class (#8)

--- Application.cs
+++ Application.cs
@@ -1,3 +1,4 @@
+using design_patterns.patterns;
 using design_patterns.patterns.builder;
 using design_patterns.patterns.Command;
 using design_patterns.patterns.Decorator;
@@ -5,13 +6,32 @@ using design_patterns.patterns.Observer;
 using design_patterns.patterns.Options;
 using design_patterns.patterns.Strategy;
 using static System.Console;
-namespace design_patterns; 
+namespace design_patterns;
 
 // TODO: Wonder if there is a nice way of optimising this?
-public class Application(IObserver observer, ICommandExample commandExample, IOptionsExample optionsExample, IDecoratorExample decoratorExample, IStrategyPatternExample strategyPatternExample
-, IBuilderExample builderExample)
+public class Application(IObserver observer, 
+    ICommandExample commandExample, 
+    IOptionsExample optionsExample, 
+    IDecoratorExample decoratorExample, 
+    IStrategyPatternExample strategyPatternExample, 
+    IBuilderExample builderExample)
 {
-    private readonly List<string> _options = ["1 - Observer", "2 - Command", "3 - Options Example", "4 - Decorator Example", "5 - Strategy Pattern", "6 - Builder Pattern", "q - Quit application"];
+    private class PatternExample(string patternName, IPatternExample patternExample)
+    {
+        public string Name { get; } = patternName;
+        public IPatternExample Pattern { get; } = patternExample;
+    }
+
+    private Dictionary<string, PatternExample> _options = new()
+    {
+        ["1"] = new("Observer", observer),
+        ["2"] = new("Command", commandExample),
+        ["3"] = new("Options Example", optionsExample),
+        ["4"] = new("Decorator Example", decoratorExample),
+        ["5"] = new("Strategy Pattern", strategyPatternExample),
+        ["6"] = new("Builder Pattern", builderExample)
+    };
+
     public void Run()
     {
         WriteLine("Design Patterns example application");
@@ -19,42 +39,37 @@ public class Application(IObserver observer, ICommandExample commandExample, IOp
         var keyboardInput = string.Empty;
         while (keyboardInput != "q")
         {
+            DisplayMenu();
+
+            keyboardInput = ReadLine();
+            bool result = _options.TryGetValue(keyboardInput!, out var patternExample);
+            
+            if (keyboardInput == "q")
+            {
+                WriteLine("Exiting...");
+                break;
+            }
+            
+            if (!result)
+            {
+                WriteLine("Invalid input");
+                continue;
+            }
+            
+            patternExample.Pattern.Run();
+        }
+
+        void DisplayMenu()
+        {
             WriteLine("Please select which design pattern to execute:");
         
             foreach (var option in _options)
             {  
-                WriteLine(option);
+                WriteLine($"{option.Key} - {option.Value.Name}");
             }
             
+            WriteLine("q - Quit Application");
             Write("> ");
-            keyboardInput = ReadLine();
-            switch (keyboardInput)
-            {
-                case "1":
-                    observer.Run();
-                    break;
-                case "2":
-                    commandExample.Run();
-                    break;
-                case "3":
-                    optionsExample.Run();
-                    break;
-                case "4":
-                    decoratorExample.Run();
-                    break;
-                case "5":
-                    strategyPatternExample.Run();
-                    break;
-                case "6":
-                    builderExample.Run();
-                    break;
-                case "q":
-                    WriteLine("Exiting application ...");
-                    break;
-                default:
-                    WriteLine("Invalid input");
-                    break;
-            }
         }
     }
 }
